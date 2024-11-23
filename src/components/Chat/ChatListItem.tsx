50cda032c420f3333103fa6b9ca41ecaf68644e1
Olexandr Lisovyi <microfox141120@outlook.com>
2024-11-23 20:43:15 +0200

    fix

--- src/components/Chat/ChatListItem.tsx
+++ src/components/Chat/ChatListItem.tsx
@@ -43,6 +43,7 @@ const ChatListItem = memo((props: IChatListItemProps) => {
       href={`/${root}/${id}`}
       onClick={() => setChat()}
       shallow
+      prefetch
       className={`chat-list-item flex transition-colors rounded-xl gap-2 w-full p-2 cursor-pointer ${
         isActive && !hideIndicators
           ? "bg-lightBlue"
