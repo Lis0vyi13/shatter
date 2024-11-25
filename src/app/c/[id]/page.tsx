9d3398e9f2387a0d572760cd811053fc5dfbd32f
Olexandr Lisovyi <microfox141120@outlook.com>
2024-11-25 22:30:02 +0200

    fix

--- src/app/c/[id]/page.tsx
+++ src/app/c/[id]/page.tsx
@@ -1,33 +1,29 @@
 "use client";
 
-import { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
-
-import useUser from "@/hooks/useUser";
-import { getChatById } from "@/services/firebase";
 
 import ChatBlock from "@/components/Chat/ChatBlock";
 
-import { IChat } from "@/types/chat";
+// export async function generateMetadata({
+//   params,
+//   searchParams,
+// }: IChatPage): Promise<Metadata> {
+//   const uid = (await searchParams).uid;
+//   const chatId = (await params).id;
+//   const chat: IChat | null = await getChatById(uid, chatId);
+//   const user = chat ? await getUserById(chat.members[0]) : null;
+
+//   return {
+//     title: uid === user?.uid ? "Favorites" : user?.displayName,
+//   };
+// }
 
 const ChatPage = () => {
   const { id } = useParams();
-  const user = useUser();
-  const [chat, setChat] = useState<IChat | null>(null);
-
-  useEffect(() => {
-    async function getChat() {
-      if (user) {
-        const currentChat = await getChatById(user?.uid, id as string);
-        setChat(currentChat);
-      }
-    }
-    getChat();
-  }, [id, user]);
 
   return (
     <div className="chat-page overflow-hidden flex flex-1">
-      <ChatBlock chat={chat} />
+      <ChatBlock id={id as string} />
     </div>
   );
 };
--- src/components/Chat/ChatBlock.tsx
+++ src/components/Chat/ChatBlock.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useEffect, useState } from "react";
+
 import useChats from "@/hooks/useChats";
 
 import Block from "@/components/ui/Block";
@@ -9,28 +11,35 @@ import Chat from "./Chat";
 
 import { IChat } from "@/types/chat";
 
-const ChatBlock = ({ chat }: { chat?: IChat | null }) => {
+const ChatBlock = ({ id }: { id?: string }) => {
+  const [activeChat, setActiveChat] = useState<IChat | null>(null);
   const chats = useChats();
+
+  useEffect(() => {
+    const chat = chats?.find((chat) => chat.id == id);
+    setActiveChat(chat || null);
+  }, [chats, id]);
+
   return (
     <Block color={"dark"} className={`chat-block flex gap-2 pr-2`}>
-      {chat ? (
+      {activeChat ? (
         <>
           <div className={`flex bg-white rounded-3xl w-full gap-2 pl-4`}>
             <div className="chat-list-wrapper min-w-[300px] h-full py-4">
               <ChatList data={chats} />
             </div>
             <div className="chat flex flex-col px-3 py-4 flex-1">
-              <Chat data={chat} />
+              <Chat data={activeChat} />
             </div>
           </div>
           <div className="chat-details-wrapper">
-            <ChatDetails data={chat} />
+            <ChatDetails data={activeChat} />
           </div>
         </>
       ) : (
         <div className={`flex bg-white rounded-3xl w-full gap-2 pl-4`}>
           <div className="chat-list w-[min(300px,_100%)] h-full py-4">
-            <ChatList data={chats || null} />
+            <ChatList data={chats} />
           </div>
           <div className="chat flex bg-gray bg-opacity-30 p-4 justify-center items-center flex-col w-full px-3 flex-1">
             <span className="px-4 py-2 text-xs text-[0.9rem] bg-dark text-white rounded-2xl">
