4a2fd8ac77dc9b84a86863741e2cba657088f88f
Olexandr Lisovyi <microfox141120@outlook.com>
2025-02-15 21:37:34 +0200

    removed base due to vercel

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  base: "./",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
